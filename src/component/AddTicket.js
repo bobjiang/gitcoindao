6a1dbc4f4973c72734f47f9e2556d7091d618037
Olayemi Oyebanji <olayemioyebanji911@gmail.com>
2022-04-12 02:04:59 +0100

    done with optimazation

---
+++ src/component/AddTicket.js
@@ -0,0 +1,55 @@
+import React, { useState, useRef } from "react";
+import { notification } from "antd";
+import { create, registerPlugin } from "filepond";
+import { FilePond } from "react-filepond";
+import FilePondPluginImageExifOrientation from "filepond-plugin-image-exif-orientation";
+import FilePondPluginImagePreview from "filepond-plugin-image-preview";
+import "../css/App.css";
+
+import "filepond/dist/filepond.min.css";
+import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
+
+registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview);
+
+const input = document.querySelector('input[type="file"]');
+
+create(input, {
+  acceptedFileTypes: ["text/html"],
+});
+
+const AddTicket = React.memo(({ setPage }) => {
+  const pond = useRef(null);
+  const [files, setFiles] = useState([]);
+  return (
+    <div className="add-ticket">
+      <FilePond
+        ref={pond}
+        files={files}
+        allowMultiple={false}
+        allowBrowse
+        allowReplace
+        name="file"
+        server={`${process.env.REACT_APP_SERVER_URL}/ticket`}
+        labelIdle='Drag & Drop or <span class="filepond--label-action">Browse</span> your files to add new ticket for upload.'
+        onupdatefiles={(fileItems) => {
+          setFiles(fileItems.map((fileItem) => fileItem.file));
+        }}
+        onprocessfile={(error) => {
+          if (error) {
+            return notification.warning({
+              message: "Error",
+              description: "File Upload was unsuccessful.",
+            });
+          }
+          setPage(1);
+          notification.success({
+            message: "Success",
+            description: "File has been Upload successfully.",
+          });
+        }}
+      />
+    </div>
+  );
+});
+
+export default AddTicket;
